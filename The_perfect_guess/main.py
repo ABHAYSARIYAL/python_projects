b588e5226b9c3a7cf8b0f0af8cf70857e43f443b
ABHAYSARIYAL <abhaysariyal143@gmail.com>
2025-05-29 18:46:25 +0530

    add projets

---
+++ The_perfect_guess/main.py
@@ -0,0 +1,18 @@
+import random
+
+k = random.randint(1,100)
+# p = int(input("guess the number between 1  : "))
+p = -1
+guess = 0
+while(p!=k):
+    p = int(input("guess the number between 1 to 100: "))
+
+    if(p>k):
+        print("lower number please")
+        guess +=1
+    else:
+        print("bigger number please")
+        guess +=1
+
+
+print(f"you guessed it right : {k} in {guess} guesses")
